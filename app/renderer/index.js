ccfb43545d770fde0d97cc295b701790c5fad740
陈醒 <czy7242@gmail.com>
2021-04-23 16:28:36 +0800

    first commit

---
+++ app/renderer/index.js
@@ -0,0 +1,14 @@
+
+// import React from 'react';
+// import ReactDOM from 'react-dom';
+
+// // import entry component
+// import Entry from './entry';
+
+// // render `Entry` component
+// ReactDOM.render(
+//     <Entry name='React'/>,
+//     document.getElementById( 'app' ),
+// );
+const appElem = document.getElementById( 'app' );
+appElem.innerHTML = '<h1>Hello World!</h1>';
